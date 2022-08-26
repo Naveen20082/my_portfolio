2cf08d3ebd2651a48c170db2b2900cadc71d464c
Naveen kumar kumawat <kumawatnaveen17@gmail.com>
2022-08-26 20:28:25 +0530

    #

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Components/files/Main"
 import About from "./Components/files/About"
 import Projects from "./Components/files/Projects"
 import Skills from "./Components/files/Skills"
+import Nav from './Components/files/Nav';
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/nav" element={<Nav />} />
+
+
       </Routes>
 
       {/* <Main /> */}
